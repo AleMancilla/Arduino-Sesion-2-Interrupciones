7c25d6c1189d71cdad52c2e7c87f8fe1f7ffb6a8
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-29 21:28:17 -0400

    interrupciones

---
+++ Ejemplo_2_interrupciones/Ejemplo_2_interrupciones.ino
@@ -0,0 +1,22 @@
+// Autor: Alejandro Mancilla Huanca - (+591)65537461
+// Contacto: alejandro.mancilla.umsa@gmail.com
+//
+const byte ledPin = 13; //definimos el pin que tendra el led
+const byte interruptPin = 2; // definimos el pin 2 que acepta interrupciones
+volatile byte state = LOW; // iniciamos un estado para controlar el estado del led
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  //definimos ledPin como pin de salida
+  pinMode(interruptPin, INPUT_PULLUP); //interruptPin como pin entrada con resistencia
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);// INTERRUPCION
+}
+
+void loop() {
+  digitalWrite(ledPin, state); // define el estado del led
+}
+
+void blink() { // funcion que se llamara al ser ejecutada la interrupcion
+  state = !state; // cambio de estado del led
+}
+
+
